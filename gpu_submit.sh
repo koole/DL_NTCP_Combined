34caec716fc394f2daad141e091b725540718f3c
Daniel MacRae <61056860+daniel-macrae@users.noreply.github.com>
2023-12-19 16:45:37 +0100

    make venv properly

--- gpu_submit.sh
+++ gpu_submit.sh
@@ -3,23 +3,21 @@
 #SBATCH --job-name=Xerostomia_1
 #SBATCH --mail-type=END
 #SBATCH --mail-user=d.macrae@student.rug.nl
-#SBATCH --time=23:59:59
+#SBATCH --time=0:59:59
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64G
+#SBATCH --mem=16G
 #SBATCH --output=slurm-%j.log
 
 
 # Install:
 module purge
-module load fosscuda/2020b
-module load OpenCV/4.2.0-foss-2020a-Python-3.8.2-contrib
-module load Python/3.8.6-GCCcore-10.2.0
+#module load fosscuda/2020b
+module load  OpenCV/4.6.0-foss-2022a-contrib
+module load Python/3.11.3-GCCcore-12.3.0
 python3 -m venv /data/$USER/.envs/HNC_env
 source /data/$USER/.envs/HNC_env/bin/activate
-# pip3 install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-pip3 install pyparsing six python-dateutil
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118pip3 install pyparsing six python-dateutil
 pip3 install torchinfo tqdm monai pytz SimpleITK pydicom scikit-image matplotlib
 pip3 install torch_optimizer
 pip3 install scikit-learn
